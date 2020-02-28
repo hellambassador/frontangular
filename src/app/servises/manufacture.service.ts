ed866388941246d8556c3868be69b1a70ad39e66
HellAmbassador <smertoroman1@yandex.ru>
2020-02-28 21:50:26 +0300

    manufacture list and delete

---
+++ src/app/servises/manufacture.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Manufacture} from "../models/manufacture";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ManufactureService {
+
+  constructor(private http: HttpClient) { }
+  getManufacturers() {
+    return this.http.get<Manufacture[]>("http://localhost:8080/api/manufactures").toPromise();
+  }
+    deleteManufacture(id:number) {
+        return this.http.delete("http://localhost:8080/api/manufacture/"+id).toPromise();
+    }
+
+}
